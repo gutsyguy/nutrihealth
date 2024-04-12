e35158f3276fa11cecb9bdbe90fdd5f5fef2ce12
gutsyguy <yalambersubba13@gmail.com>
2024-04-12 16:27:07 -0700

    Initial Commit

---
+++ power-plant/power_plantApp.swift
@@ -0,0 +1,17 @@
+//
+//  power_plantApp.swift
+//  power-plant
+//
+//  Created by user on 4/12/24.
+//
+
+import SwiftUI
+
+@main
+struct power_plantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
